67a7dcc5871e944086c0adc6580fc42a6844376e
Vandhuy F Martins <vandhuy@gmail.com>
2018-05-03 15:24:05 -0300

    [Login] O password está sendo digitado duas vezes #8

--- helpers/modules/login/index.js
+++ helpers/modules/login/index.js
@@ -1,41 +1,34 @@
 function loginHelpers(config) {
   const _EC = protractor.ExpectedConditions;
 
+  const _usernameInput = element(by.id('986fff3e-3acb-405a-ae08-a4044481b962'));
+  const _passwordInput = element(by.id('9aa3d436-2a70-4480-bde8-fd04a448618f'));
+  const _enterButton = element(by.id('ed133c12-8c21-4313-a55a-7cd0c772cc7f'));
+  const _logOutButton = element(by.id('4aa30caf-e786-4219-8e46-50d399e03eaf'));
+
   return {
     logInWithUsernameAndPassword: function(username, password) {
-      const usernameInput = element(by.id('986fff3e-3acb-405a-ae08-a4044481b962'));
-
-      browser.wait(_EC.visibilityOf(usernameInput))
+      browser.wait(_EC.visibilityOf(_usernameInput))
         .then(function() {
-          element(by.id('986fff3e-3acb-405a-ae08-a4044481b962'))
-            .sendKeys(username);
-          element(by.id('9aa3d436-2a70-4480-bde8-fd04a448618f'))
-            .sendKeys(password);
-
-          element(by.id('ed133c12-8c21-4313-a55a-7cd0c772cc7f')).click();
+          _usernameInput.sendKeys(username);
+          _passwordInput.sendKeys(password);
+          _enterButton.click();
         });
     },
 
     logInWithRole: function(role) {
-      const usernameInput = element(by.id('986fff3e-3acb-405a-ae08-a4044481b962'));
-
-      browser.wait(_EC.visibilityOf(usernameInput))
+      browser.wait(_EC.visibilityOf(_usernameInput))
         .then(function() {
-          usernameInput.sendKeys(config.internals.roles[role].username);
-
-          element(by.id('9aa3d436-2a70-4480-bde8-fd04a448618f'))
-            .sendKeys(config.internals.roles[role].password);
-
-          element(by.id('ed133c12-8c21-4313-a55a-7cd0c772cc7f')).click();
+          _usernameInput.sendKeys(config.internals.roles[role].username);
+          _passwordInput.sendKeys(config.internals.roles[role].password);
+          _enterButton.click();
         });
     },
 
     logOut: function() {
-      const logOutButton = element(by.id('4aa30caf-e786-4219-8e46-50d399e03eaf'));
-
-      browser.wait(_EC.elementToBeClickable(logOutButton))
+      browser.wait(_EC.elementToBeClickable(_logOutButton))
         .then(function() {
-          logOutButton.click();
+          _logOutButton.click();
         });
     }
   };
