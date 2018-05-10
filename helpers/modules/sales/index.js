2a6668beee728f10c38ae52d8e016f232a37ecb9
Vandhuy F Martins <vandhuy@gmail.com>
2018-05-10 14:48:13 -0300

    O método disableHelpModalIfVisible não está fechando o modal #18

--- helpers/modules/sales/index.js
+++ helpers/modules/sales/index.js
@@ -1,23 +1,15 @@
 function salesHelpers(identifiers) {
 
-  const _delayBootstrapModal = 1000; // Delay to the Twitter Bootstrap Modal open and close;
-
   return {
     disableHelpModalIfVisible: function() {
-      browser.sleep(_delayBootstrapModal);
-
       const _salesPage = identifiers.elements.salesPage;
 
-      const _showWhenOpenedSelect = element(by.id(_salesPage.showWhenOpenedSelect));
       const _closeHelpModalButton = element(by.id(_salesPage.closeHelpModalButton));
 
-      _showWhenOpenedSelect.isDisplayed()
+      _closeHelpModalButton.isDisplayed()
         .then(isVisible => {
           if (isVisible) {
-            _showWhenOpenedSelect.click();
             _closeHelpModalButton.click();
-
-            browser.sleep(_delayBootstrapModal);
           }
 
           return isVisible;
